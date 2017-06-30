4e5bfbc2ce59ce67654042f751c0acd0380a5020
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-06-30 08:56:35 -0400

    Renamed project files to match naming convention. Updated assembly info.

--- MVCGridExample/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCGrid.Web.MvcApplication" Language="C#" %>
---
+++ MichaelBrandonMorris.MvcGrid/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MichaelBrandonMorris.MvcGrid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michael Brandon Morris")]
+[assembly: AssemblyProduct("MichaelBrandonMorris.MvcGrid")]
+[assembly: AssemblyCopyright("Copyright © Michael Brandon Morris 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3376a19f-07d4-4a66-8844-30bb53c1204f")]
+[assembly: AssemblyVersion("1.0.*")]
